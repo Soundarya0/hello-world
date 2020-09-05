ac9ae70a6b26db32f61d18c47529ed700f2bfe87
Soundarya0 <soundaryaplm@gmail.com>
2020-09-05 06:55:33 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+export default App;
